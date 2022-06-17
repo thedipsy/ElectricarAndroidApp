53289372beae1e48902b38cc3732eaf7bed27bab
Angela Milosheska <milosheskaa@gmail.com>
2022-06-17 22:19:23 +0200

    Initial commit

---
+++ app/src/main/java/com/electricarandroidapp/view/activities/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.electricarandroidapp.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.electricarandroidapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
